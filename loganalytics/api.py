130ba5cb8d67fb1b76917d18f281d19031877b52
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2025-09-30 17:59:19 +0530

    error handling and status update changes

--- loganalytics/api.py
+++ loganalytics/api.py
@@ -4,32 +4,35 @@ from frappe.utils import now, get_datetime
 
 @frappe.whitelist()
 def sync_log_data():
-    """
-    Incremental sync from remote sites into local doctypes.
-    Uses 'modified' field for filtering.
-    """
 
     la_settings = frappe.get_doc("LA Settings")
     sites = la_settings.sites
     table_settings = la_settings.table_settings
+
+    if len(sites) == 0:
+        raise Exception("Missing site(s) in LA Settings")
+    if len(table_settings) == 0:
+        raise Exception("Missing table(s) in LA Settings")
+    
     for site in sites:
         remote_site = site.site_url
         api_key = site.api_key
         api_secret = site.api_secret
 
         for table_setting in table_settings:
-            last_sync = table_setting.last_sync_date
+            records_synced_till = table_setting.records_synced_till
             url=f"{remote_site}/api/resource/{table_setting.source_table}"
             params = {
                 "fields": '["name", "creation", "modified", "route", "user"]',
-                "filters": f'[["modified", ">", "{last_sync}"]]'
+                "filters": f'[["modified", ">", "{records_synced_till}"]]'
             }
             headers={
                 "Authorization": f"token {api_key}:{api_secret}"
                 }
             response = requests.get(url=url, headers=headers, params=params)
             record_list = response.json()["data"]
-            max_modified = last_sync
+            max_modified = records_synced_till
+            records_synced_in_last_run = 0
             for record in record_list:
                 frappe.get_doc({
                     "doctype": table_setting.target_table,
@@ -40,11 +43,15 @@ def sync_log_data():
                     "route": record["route"],
                     "user": record["user"],
                 }).insert(ignore_if_duplicate=True)
+                records_synced_in_last_run = records_synced_in_last_run+1
 
                 if get_datetime(record["modified"]) > get_datetime(max_modified):
                     max_modified = record["modified"]
-            # update last_sync_date
-            table_setting.db_set("last_sync_date", max_modified)
-    return {"status": "success", "synced_on": now()}
+
+            table_setting.db_set("records_synced_till", max_modified)
+            table_setting.db_set("records_synced_in_last_run", records_synced_in_last_run)
+            table_setting.db_set("last_sync_job_run_date", now())
+
+    return {"status": "success", "last_sync_job_run_date": now()}
 
   
